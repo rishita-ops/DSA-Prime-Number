b47ac2a4e8d79837b12f7ae31c4f7a45b2714325
rishita-ops <rishita.sanghavi07@gmail.com>
2026-03-12 15:09:38 +0530

    First Commit

---
+++ code.cpp
@@ -0,0 +1,28 @@
+//Check Prime or not
+#include <iostream>
+using namespace std;
+int main()
+{    int n, i, flag = 0;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    for (i = 2; i <= n / 2; ++i)
+    {
+        if (n % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (n == 1)
+    {
+        cout << "1 is neither prime nor composite.";
+    }
+    else
+    {
+        if (flag == 0)
+            cout << n << " is a prime number.";
+        else
+            cout << n << " is not a prime number.";
+    }
+    return 0;
+}
